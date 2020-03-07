e5e1fc67ad3633f8aea721e64b46c88560a2da46
di2mot <motsar.dm@gmail.com>
2020-03-07 22:11:52 +0200

    Update Rheology.py
    Поправил ошибки

--- Rheology.py
+++ Rheology.py
@@ -10,22 +10,30 @@ class Rheology:
         # __coef  - коэфициент перевода единиц
         self.__coef = 0.48
         # fann600 -
-        self.rpm_600 = rpm_600
+        self.rpm_600 = 0
         # fann300 -
-        self.rpm_300 = rpm_300
+        self.rpm_300 = 0
         # fann200 -
-        self.rpm_200 = rpm_00
+        self.rpm_200 = 0
         # fann100 -
-        self.rpm_100 = rpm_100
+        self.rpm_100 = 0
         # fann60 -
-        self.rpm_60 = rpm_60
+        self.rpm_60 = 0
         # fann30 -
-        self.rpm_30 = rpm_0
+        self.rpm_30 = 0
         # fann6 -
-        self.rpm_6 = rpm_6
+        self.rpm_6 = 0
         # fann3 -
-        self.rpm_3 = rpm_3
+        self.rpm_3 = 0
+        # sns
+        self.sns_10s = 0
+        self.sna_10m = 0
 
-    def sns(self, sns):
+    def sns_1(self):
         ''' For SNS  calculation'''
-        return sns * self.__coef
+        return self.sns_10s * self.__coef, self.sns_10m * self.__coef
+
+    def reo(self):
+        PV = self.rpm_600 - self.rpm_300
+        YV = (PV - self.rpm_300) * self.__coef
+        return PV, YV
