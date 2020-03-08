19057f6f2d010c53c9ce8fc2f38b2f8ca443b72a
di2mot <motsar.dm@gmail.com>
2020-03-08 14:06:10 +0200

    Update Rheology.py

--- Rheology.py
+++ Rheology.py
@@ -5,36 +5,34 @@
 class Rheology:
     '''For rheology  calculation'''
 
-    def __init__(self, *args):
+    def __init__(self, kwargs):
 
         # __coef  - conversion factor
         # __coef  - коэфициент перевода единиц
         self.__coef = 0.48
         # fann600 -
-        self.rpm_600 = args['600 prm']
+        self.rpm_600 = kwargs['600 prm']
         # fann300 -
-        self.rpm_300 = args['300 prm']
+        self.rpm_300 = kwargs['300 prm']
         # fann200 -
-        self.rpm_200 = 0
+        self.rpm_200 = kwargs['200 prm']
         # fann100 -
-        self.rpm_100 = 0
+        self.rpm_100 = kwargs['100 prm']
         # fann60 -
-        self.rpm_60 = 0
-        # fann30 -
-        self.rpm_30 = 0
+        # self.rpm_60 = kwargs['60 prm']
+        # # fann30 -
+        # self.rpm_30 = kwargs['30 prm']
         # fann6 -
-        self.rpm_6 = 0
+        self.rpm_6 = kwargs['6 prm']
         # fann3 -
-        self.rpm_3 = 0
-        # sns
-        self.sns_10s = 0
-        self.sna_10m = 0
+        self.rpm_3 = kwargs['3 prm']
 
-    def sns_1(self):
+
+    def sns_1(self, sns_10s, sns_10m):
         ''' For SNS  calculation'''
-        return self.sns_10s * self.__coef, self.sns_10m * self.__coef
+        return sns_10s * self.__coef, sns_10m * self.__coef
 
     def reo(self):
-        PV = self.rpm_600 - self.rpm_300
-        YV = (self.rpm_300 - PV) * self.__coef
-        return PV, YV
+        pv = self.rpm_600 - self.rpm_300
+        yv = (self.rpm_300 - pv) * self.__coef
+        return pv, yv
