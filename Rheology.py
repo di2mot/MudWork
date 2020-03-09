be6b9dbd098c9e0543ff93f16ed06163d9f7d565
di2mot <motsar.dm@gmail.com>
2020-03-09 20:51:07 +0200

    Update Rheology.py

--- Rheology.py
+++ Rheology.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import math
+import numpy
 
 class Rheology:
     '''For rheology  calculation'''
@@ -32,7 +33,64 @@ class Rheology:
         ''' For SNS  calculation'''
         return sns_10s * self.__coef, sns_10m * self.__coef
 
-    def reo(self):
-        pv = self.rpm_600 - self.rpm_300
-        yv = (self.rpm_300 - pv) * self.__coef
-        return pv, yv
+    def reo(self, model = 'hb'):
+        ''' Type of models:
+Bingham Plastic Model (bingham)—This model describes fluids in which the shear stress/shear rate ratio is linear
+once a specific shear stress has been exceeded.
+
+Power Law (power_low) —The Power Law is used to describe the flow of shear thinning or pseudoplastic drilling
+fluids.
+
+Herschel-Bulkley Model (bh) —Also called the “modified” power law and yield-pseudoplastic model, the
+Herschel-Bulkley model is used to describe the flow of pseudoplastic drilling fluids which require a yield stress to
+initiate flow. Defoult model
+        '''
+        _list = {}
+        if model == 'hb':
+
+            plastic_viscosity = self.rpm_600 - self.rpm_300
+            _list['plastic_viscosity'] = plastic_viscosity
+
+            yield_point = (self.rpm_300 - plastic_viscosity) * self.__coef
+            _list['yield_point'] = yield_point
+
+            yield_stress = 2*self.rpm_3 - self.rpm_6
+            _list['yield_stress'] = yield_stress
+
+            flow_index = 3.32*math.log10((self.rpm_600- yield_stress)/(self.rpm_300 - yield_stress))
+            _list['flow_index'] = flow_index
+
+            consistency_index = (self.rpm_300 - yield_stress)/511**flow_index
+            _list['consistency_index'] = consistency_index
+
+            return _list
+
+        elif model == 'power_low':
+
+            plastic_viscosity = self.rpm_600 - self.rpm_300
+            _list['plastic_viscosity'] = plastic_viscosity
+
+            yield_point = (self.rpm_300 - plastic_viscosity) * self.__coef
+            _list['yield_point'] = yield_point
+
+            yield_stress = 2 * self.rpm_3 - self.rpm_6
+            _list['yield_stress'] = yield_stress
+
+            _list.append(plastic_viscosity, yield_point, yield_stress)
+
+            '''For Pipe flow'''
+            pipe_flow_flow_index = 3.32 * math.log10((self.rpm_600) / (self.rpm_300))
+            _list['pipe_flow_flow_index'] = pipe_flow_flow_index
+
+            pipe_flow_consistency_index = (self.rpm_300) / 511 ** pipe_flow_flow_index
+            _list['pipe_flow_consistency_index'] = pipe_flow_consistency_index
+
+            '''For Annular flow  '''
+            annular_flow_flow_index = 3.32 * math.log10((self.rpm_600 - yield_stress) / (self.rpm_300 - yield_stress))
+            _list['annular_flow_flow_index'] = annular_flow_flow_index
+
+            annular_flow_consistency_index = (self.rpm_300 - yield_stress) / 511 ** annular_flow_flow_index
+            _list['annular_flow_consistency_index'] = annular_flow_consistency_index
+
+            return _list
+
