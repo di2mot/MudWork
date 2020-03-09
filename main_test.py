b4dd86029f335744a2b784ae539652bc5b1c8c5e
di2mot <motsar.dm@gmail.com>
2020-03-09 22:51:22 +0200

    Update main_test.py
    add main() loop and
     if __name__ =='__main__'
    Litle update

--- main_test.py
+++ main_test.py
@@ -1,81 +1,106 @@
 import Rheology
 
 '''
-reo() - на данный момент основная функция расчёта реологических параметров бурового рствора
+reo() - на данный момент основная функция расчёта
+реологических параметров бурового рствора
 '''
-def reo(_fann):
+
+
+def reo(_fann, model):
     rh = Rheology.Rheology(_fann)
-    reo_res = rh.reo()
+    reo_res = rh.reo(model)
     return reo_res
     # sns_res = rh.sns_1()
 
 
-'''
-Здесь находится логика отвечающая за взаимодействие в "программе"
-Писутствует два варианта, для тестирования и для непосредственного ввода значений.
+def beautyTable(reo_res):
+    max_lenth = 0
+    for i in reo_res:
+        if len(i) >= max_lenth:
+            max_lenth = len(i)
+
+    for res in reo_res:
+        print(res.replace('_', ' ').capitalize().ljust(
+            max_lenth + 5), f'{reo_res[res]:.4f}')
+
+
+def stop():
+
+    print('\nДля заверщения работы введите "s", \
+    для продолжения нажмите "Enter"\n')
+    next_choise = input('Введите: ').lower()
+    if next_choise == 's':
+        print('Ввод завершён')
+        return exit()
+
+
+def main():
+    '''
+    Здесь находится логика отвечающая за взаимодействие в "программе"
+Писутствует два варианта, для тестирования и для непосредственного
+ввода значений.
 Для тестирования достаточно ввести "t"
 Для ввода значений нужно ввести "w"
 Для остановки работы и вохода "s"
-'''
-while True:
-    print('')
-    choise = input('Для теста введите "t", для ввода введите "w": ').lower()
-    print('')
+    '''
 
-    # Функция для тестирования
-    if choise == 't':
-        _fann = {'600 prm': 100, '300 prm': 81, '200 prm': 72, '100 prm': 62, '6 prm': 48, '3 prm': 47}
-        for i in _fann:
-            print(f'{i}:\t{_fann[i]}')
-        print('')
+    while True:
 
-        # оотправляем заначения, получаем результат
-        model = 'power_low'
-        reo_res = reo(_fann, model)
-        for res in reo_res:
-            print(res, '\t', f'{reo_res[res]:.4f}')
+        choise = input(
+            '\nДля теста введите "t", для ввода введите "w": ').lower()
 
-        print('')
-        print('Для заверщения работы введите "s", для продолжения нажмите "Enter"')
-        next_choise = input('Введите: ').lower()
-        if next_choise == 's':
-            print('Ввод завершён')
-            break
+        # Функция для тестирования
+        if choise == 't':
+            _fann = {'600 prm': 100, '300 prm': 81, '200 prm': 72,
+                     '100 prm': 62, '6 prm': 48, '3 prm': 47}
+            beautyTable(_fann)
+            print('')
 
-    # Функция для работы
-    elif choise == 'w':
-        _fann = {'600 prm': 0, '300 prm': 0, '200 prm': 0, '100 prm': 0, '6 prm': 0, '3 prm': 0}
-        for i in _fann:
+            # отправляем заначения, получаем результат
+            model = 'pl'
+            reo_res = reo(_fann, model)
+            beautyTable(reo_res)
+
+            stop()
+
+        # Функция для работы
+        elif choise == 'w':
+            _fann = {'600 prm': 0, '300 prm': 0, '200 prm': 0,
+                     '100 prm': 0, '6 prm': 0, '3 prm': 0}
+            for i in _fann:
+                while True:
+                    try:
+                        _fann[i] = int(input(f'Введите значение для {i}: '))
+                        break
+                    except ValueError:
+                        print('\nВводить можно только целые значения\n')
+
+            # оотправляем заначения, получаем результат
+            text = '\nВыберите реологическую модель:\n\
+            bh - Bingham Plastic Model (Not work)\n\
+            pl - Power Law Model\n\
+            hb - Herschel-Bulkley Model  \n\
+            Ввод: '
+
             while True:
                 try:
-                    _fann[i] = int(input(f'Введите значение для {i}: '))
+                    model = str(input(text))
+                    reo_res = reo(_fann, model)
+                    beautyTable(reo_res)
+                    stop()
                     break
                 except ValueError:
-                    print('')
-                    print('Вводить можно только целые значения')
+                    print('\nВводить можно только текст\n')
 
-        print('')
 
-        # оотправляем заначения, получаем результат
-        reo_res = reo(_fann)
-        for res in reo_res:
-            print(res, '\t', f'{reo_res[res]:.4f}')
 
-        print('')
-        print('Для заверщения работы введите "s", для продолжения нажмите "Enter"')
-        next_choise = input('Введите: ').lower()
-        if next_choise == 's':
-            print('Ввод завершён')
+        elif choise == 's':
+            print('\nРабота завершена')
             break
 
-    else:
-        print('')
-        print('Неверный ввод')
-        choise = input('Для теста введите "t", для ввода введите "w"').lower()
+        else:
+            print('Неверный ввод')
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    main()
