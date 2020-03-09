5df7607810d3ccec982304b2308736da72c4efd2
di2mot <motsar.dm@gmail.com>
2020-03-09 20:51:02 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,25 +1,46 @@
 import Rheology
 
-
+'''
+reo() - на данный момент основная функция расчёта реологических параметров бурового рствора
+'''
 def reo(_fann):
     rh = Rheology.Rheology(_fann)
     reo_res = rh.reo()
+    return reo_res
     # sns_res = rh.sns_1()
-    print(f'Значение PV:\t {reo_res[0]}')
-    print(f'Значение YV:\t {reo_res[1]:.2f}')
-    # print('Значение СНС: ', sns_res[0], '/', sns_res[1])
 
 
+'''
+Здесь находится логика отвечающая за взаимодействие в "программе"
+Писутствует два варианта, для тестирования и для непосредственного ввода значений.
+Для тестирования достаточно ввести "t"
+Для ввода значений нужно ввести "w"
+Для остановки работы и вохода "s"
+'''
 while True:
     print('')
-    choise = input('Для теста введите "t", для ввода введите "w" :').lower()
+    choise = input('Для теста введите "t", для ввода введите "w": ').lower()
     print('')
 
     # Функция для тестирования
     if choise == 't':
         _fann = {'600 prm': 100, '300 prm': 81, '200 prm': 72, '100 prm': 62, '6 prm': 48, '3 prm': 47}
-        reo(_fann)
+        for i in _fann:
+            print(f'{i}:\t{_fann[i]}')
+        print('')
 
+        # оотправляем заначения, получаем результат
+        model = 'power_low'
+        reo_res = reo(_fann, model)
+        for res in reo_res:
+            print(res, '\t', f'{reo_res[res]:.4f}')
+
+        print('')
+        print('Для заверщения работы введите "s", для продолжения нажмите "Enter"')
+        next_choise = input('Введите: ').lower()
+        if next_choise == 's':
+            print('Ввод завершён')
+            break
 
     # Функция для работы
     elif choise == 'w':
@@ -27,15 +48,19 @@ while True:
         for i in _fann:
             while True:
                 try:
-                    _fann[i] = int(input(f'Введите значение для {i}:'))
+                    _fann[i] = int(input(f'Введите значение для {i}: '))
                     break
                 except ValueError:
                     print('')
                     print('Вводить можно только целые значения')
-                    #_fann[i] = int(input(f'Введите значение для {i}:'))
 
         print('')
-        reo(_fann)
+
+        # оотправляем заначения, получаем результат
+        reo_res = reo(_fann)
+        for res in reo_res:
+            print(res, '\t', f'{reo_res[res]:.4f}')
+
         print('')
         print('Для заверщения работы введите "s", для продолжения нажмите "Enter"')
         next_choise = input('Введите: ').lower()
@@ -47,6 +72,7 @@ while True:
         print('')
         print('Неверный ввод')
         choise = input('Для теста введите "t", для ввода введите "w"').lower()
+
 
 
 
